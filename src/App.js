8e42e3d99fe7b9000c6c87baf3e7aed9e18ea1c9
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-05 13:44:33 -0600

    option images load

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import OptionBox from "./components/OptionBox";
+import rock from "./images/rock.png";
+import paper from "./images/paper.png";
+import scissors from "./images/scissors.png";
+import { Container, Header, } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { userChoice: null, compChoice: null, };
+
+  optionClick = (option) => {
+    debugger
+    // set state of optionClick to be rock
+  }
+
+  render() {
+    return (
+      <Container style={{ marginTop: "25px", }}>
+        <Header as="h1">React Rock Paper Scissors</Header>
+        <hr />
+        <OptionBox name="rock" img={rock} optionClick={this.optionClick} />
+        <OptionBox name="paper" img={paper} />
+        <OptionBox name="scissors" img={scissors} />
+      </Container>
+    );
+  };
+};
 
 export default App;
---
+++ src/components/OptionBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image, } from "semantic-ui-react";
+
+const OptionBox = (props) => (
+  <Image 
+    src={props.img} 
+    size="small" 
+    onClick={() => props.optionClick(props.name)} 
+  />
+);
+
+export default OptionBox;
