6937abdea170f87f4319ad49f403717d926b6bec
Alfreda Tsai <alfredatsai@gmail.com>
2019-06-05 15:59:10 -0600

    user and comp Choice assigned

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import OptionBox from "./components/OptionBox";
 import rock from "./images/rock.png";
 import paper from "./images/paper.png";
 import scissors from "./images/scissors.png";
+// import Results from "./components/Results"
 import { Container, Header, } from "semantic-ui-react";
 
 class App extends React.Component {
   state = { userChoice: null, compChoice: null, };
 
+  randomChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+  }
+
   optionClick = (option) => {
-    debugger
-    // set state of optionClick to be rock
+    this.setState({ userChoice: option, compChoice: this.randomChoice() })
   }
 
   render() {
     return (
       <Container style={{ marginTop: "25px", }}>
-        <Header as="h1">React Rock Paper Scissors</Header>
+        <Header as="h1" textAlign="center">React Rock Paper Scissors</Header>
         <hr />
         <OptionBox name="rock" img={rock} optionClick={this.optionClick} />
-        <OptionBox name="paper" img={paper} />
-        <OptionBox name="scissors" img={scissors} />
+        <OptionBox name="paper" img={paper} optionClick={this.optionClick} />
+        <OptionBox name="scissors" img={scissors} optionClick={this.optionClick} />
+        <hr />
+        
+        {/* <Results 
+          user={this.state.userChoice}
+          comp={this.state.compChoice}
+        /> */}
+
       </Container>
     );
   };
---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header, } from "semantic-ui-react";
+
+const Results = ( user, comp, ) => (
+
+)
+
+{/* <Header>
+  {this.state.userChoice}
+</Header> */}
+
+export default Results;
